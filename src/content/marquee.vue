4cc84f711e563cb871a84c78b1dffecf65333c29
kelvin <kelvinho84@gmail.com>
2018-05-31 15:39:41 +0800

    bug fix on marquee

---
+++ src/content/marquee.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <marquee behavior="scroll" direction="left">
+      <div v-for="crypto in cryptos">
+        <a :href="cmLink(crypto)" target="_blank">
+          {{crypto.symbol}} <span :class="crypto.percent_change_1h > 0 ? 'green':'red'">
+            <span v-if="useUSD">${{myPrice(crypto)}} </span>
+            <span v-else> B{{crypto.price_btc}} </span>
+          </span>
+        </a>
+      </div>
+    </marquee>
+    <div class="actions">
+      <a href="#" @click="cancelMarquee"><i class="el-icon-close"></i></a>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['cryptos', 'useUSD', 'useRedUp', 'currencyDisplay', 'currencyLowerCase'],
+    data: () => ({
+    }),
+    computed: {
+    },
+    created () {
+      document.body.className += ' ' + 'bitome'
+      console.log('2223')
+    },
+    mounted () { },
+    methods: {
+      cmLink (crypto) {
+        return `https://coinmarketcap.com/currencies/${crypto.name.toLowerCase()}`
+      },
+      myPrice (crypto) {
+        if (this.currencyDisplay !== 'USD') {
+          return crypto[`price_${this.currencyLowerCase}`] || `USD ${crypto.price_usd}`
+        }
+        return crypto.price_usd
+      },
+      cancelMarquee () {
+        this.$emit('cancelMarquee', false)
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  marquee {
+    font-size: 16px !important;
+    height: 18px !important;
+    line-height: 18px !important;
+    a {
+      text-decoration: none !important;
+      color: black !important;
+    }
+  }
+
+  div {
+    font-size: 16px !important;
+    height: 18px !important;
+    line-height: 18px !important;
+    display: inline-block;
+    span {
+      padding-right: 8px;
+      color: #f34146 !important;
+      &.green {
+        color: #81c14f !important;
+      }
+    }
+  }
+  .actions{
+    position: fixed;
+    top: 0;
+    right: 0;
+    background: white;
+  }
+  .bitome {
+    nav, header, .header,
+    #facebook ._-kb div {
+      margin-top: 18px !important;
+    }
+  }
+</style>
---
+++ src/content/root.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="top-bar" :class="marquee ? 'active' : ''" v-if="marquee">
+    <Marquee
+      :cryptos="joinCryptos"
+      :useUSD="useUSD"
+      :useRedUp="settings.useRedUp"
+      :currencyDisplay="currencyDisplay"
+      :currencyLowerCase="currencyLowerCase"
+      v-on:cancelMarquee="cancelMarquee"
+    />
+  </div>
+</template>
+<script>
+import _isEmpty from 'lodash/isEmpty'
+
+import API from '../api'
+import CONFIG from '../utils/config'
+import store from '../ext/storage'
+import Marquee from './marquee.vue'
+const CRYPTO_DB_NAME = 'mycrypto'
+const SETTING_DB_NAME = 'settings'
+
+export default {
+  data: () => ({
+    cryptos: [],
+    userCryptos: [],
+    allCryptos: [],
+    useUSD: true,
+    useRedUp: false,
+    marquee: false,
+    currentCurrency: {
+      value: 'USD'
+    },
+    currencys: CONFIG.currencys
+  }),
+  computed: {
+    joinCryptos () {
+      return {
+        ...this.userCryptos,
+        ...this.cryptos
+      }
+    },
+    settings () {
+      const setting = {
+        useRedUp: this.useRedUp,
+        useUSD: this.useUSD,
+        currentCurrency: this.currentCurrency,
+        marquee: this.marquee
+      }
+      return setting
+    },
+    currencyLowerCase () {
+      return this.currentCurrency.value.toLowerCase()
+    },
+    currencyDisplay () {
+      return this.currentCurrency.value
+    }
+  },
+  created () {
+    const that = this
+    this.getTop10()
+    setInterval(this.loadSettings, 5 * 1000)
+    setInterval(this.getTop10, 60 * 1000)
+    store.get(CRYPTO_DB_NAME)
+      .then((db) => {
+        if (!_isEmpty(db[CRYPTO_DB_NAME])) {
+          that.userCryptos = db[CRYPTO_DB_NAME]
+        }
+      })
+  },
+  mounted () { },
+  methods: {
+    saveSettings () {
+      const setting = {
+        useRedUp: this.useRedUp,
+        useUSD: this.useUSD,
+        currentCurrency: this.currentCurrency,
+        marquee: this.marquee
+      }
+      store.set(SETTING_DB_NAME, setting)
+    },
+    cancelMarquee () {
+      this.marquee = false
+      this.saveSettings()
+    },
+    loadSettings () {
+      const that = this
+      store.get(SETTING_DB_NAME)
+        .then((db) => {
+          if (JSON.stringify(this.settings) === JSON.stringify(db[SETTING_DB_NAME])) {
+            return
+          }
+          if (!_isEmpty(db[SETTING_DB_NAME])) {
+            if ('useRedup' in db[SETTING_DB_NAME]) {
+              that.useRedUp = db[SETTING_DB_NAME].useRedUp
+            }
+            if ('currentCurrency' in db[SETTING_DB_NAME]) {
+              that.currentCurrency = db[SETTING_DB_NAME].currentCurrency
+            }
+            if ('useUSD' in db[SETTING_DB_NAME]) {
+              that.useUSD = db[SETTING_DB_NAME].useUSD
+            }
+            if ('marquee' in db[SETTING_DB_NAME]) {
+              that.marquee = db[SETTING_DB_NAME].marquee
+            }
+          }
+        })
+    },
+    getTop10 () {
+      const that = this
+      API.Crypto.getCryptoPrice().then(response => {
+        that.cryptos = response.data
+      }).catch(e => {
+        this.error = e
+      })
+    }
+  },
+  components: {
+    Marquee
+  }
+}
+</script>
+<style lang="scss">
+  .top-bar.active {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    min-height: 18px;
+    height: 18px;
+    background: white;
+    z-index: 999999999;
+  }
+</style>
